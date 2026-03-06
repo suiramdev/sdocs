a65bb4104497b33d76faf6b69a211a796410b9d7
suiramdev <me@suiram.dev>
2026-03-06 11:56:54 +0100

    Add development callouts to docs

--- apps/fumadocs/src/features/docs/components/mdx-components.tsx
+++ apps/fumadocs/src/features/docs/components/mdx-components.tsx
@@ -1,5 +1,4 @@
 import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
-import { Callout } from "fumadocs-ui/components/callout";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
@@ -11,7 +10,6 @@ export const getMDXComponents = (
 ): MDXComponents => ({
   Accordion,
   Accordions,
-  Callout,
   McpCursorInstallButton,
   Tab,
   Tabs,
